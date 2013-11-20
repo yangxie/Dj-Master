cefd12c3e244c4d4ceaa4accf9f9241647167bf7
Eric Leung <eric.leung@fivestarscard.com>
2013-11-20 15:45:05 -0800

    get more of the shit working

--- simpleapp/simpleapp.js
+++ simpleapp/simpleapp.js
@@ -1,3 +1,29 @@
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13 ||
+                 evt.type === "focusout") {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+
+  return events;
+};
+
+
 if (Meteor.isClient) {
 
     Template.loggedOutTemplate.events({
@@ -5,24 +31,32 @@ if (Meteor.isClient) {
         'submit #login-form' : function(e, t){
             e.preventDefault();
             // retrieve the input field values
-            var email = t.find('#login-email').value
-        , password = t.find('#login-password').value;
-    // Trim and validate your fields here.... 
+            var email = t.find('#login-email').value;
+            var password = t.find('#login-password').value;
+            // Trim and validate your fields here.... 
 
-    // If validation passes, supply the appropriate fields to the
-    // Meteor.loginWithPassword() function.
-    Meteor.loginWithPassword(email, password, function(err){
-        if (err)
-        console.log("not logged in");
-    // The user might not have been found, or their passwword incorrect
-    // could be incorrect. Inform the user that their
-    // login attempt has failed. 
-        else
-        window.location = "/";
-    // The user has been logged in.
-    });
-    return false; 
+            // If validation passes, supply the appropriate fields to the
+            // Meteor.loginWithPassword() function.
+            Meteor.loginWithPassword(email, password, function(err){
+                if (err)
+                console.log("not logged in");
+            // The user might not have been found, or their passwword incorrect
+            // could be incorrect. Inform the user that their
+            // login attempt has failed. 
+                else
+                window.location = "/";
+            // The user has been logged in.
+            });
+            return false; 
         },
+
+        'click #register-button': function(e, t) {
+            var email = t.find('#login-email').value;
+            var password = t.find('#login-password').value;  
+            Accounts.createUser({email: email, password: password}, function(){
+                window.location = "/";
+            });
+        }
 
     });
 
@@ -33,6 +67,34 @@ if (Meteor.isClient) {
         }
 
     });
+
+
+    Template.roomsTemplate.events(okCancelEvents(
+    '#new-room',
+  {
+    ok: function (name, evt) {
+      if (Rooms.findOne({"name": name})){
+        alert("room already exists");
+        return;
+      }
+      Rooms.insert({
+        name: name,
+        created_by: Meteor.user()
+      });
+      evt.target.value = '';
+    }
+  }));
+
+    Template.roomsTemplate.events({
+    'click .join-button': function(e) {
+        Session.set("currentRoom", $(e.currentTarget).attr("data-name"));
+        $("#content").html(Template.roomTemplate({'name': Session.get("currentRoom")}));
+    }});
+
+
+    Template.roomsTemplate.rooms = function() { 
+        return Rooms.find();
+    }
 }
 
 if (Meteor.isServer) {
